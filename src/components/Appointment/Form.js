f09d60871bbb4cb8115ab2f1d5a5eec3e42772f6
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-07-09 01:00:31 -0600

    form added

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+import InterviewerList from "components/InterviewList";
+import Button from "components/Button";
+
+
+export default function Form(props) {
+
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+
+return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            value={name}
+            className="appointment__create-input text--semi-bold"
+            name={name}
+            type="text"
+            onChange={(event) => {setName(event.target.value)}}
+          />
+        </form>
+        <InterviewerList interviewers={props.interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={ props.onCancel }>Cancel</Button>
+          <Button confirm onClick={ props.onSave }>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+
+}
