887930f6f423051a2da9f7e46a013c29c13cd698
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-07-09 02:43:43 -0600

    added displaying appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { Fragment } from 'react';
+import ReactDOM from "react-dom";
+
+import Header from "components/Appointment/Header"
+import Show from "components/Appointment/Show"
+import Empty from "components/Appointment/Empty"
 
 
 
@@ -7,6 +12,13 @@ import "components/Appointment/styles.scss";
 
 export default function Appointment (props) {
   return (
-    <article className="appointment">Hello</article>
-  );
+    <>
+    <Header time={props.time} />
+    { !props.interview && <Empty /> }
+    { props.interview && <Show student={props.interview.student} interviewer={props.interview.interviewer}/> }
+    
+    </>
+    );
 }
+
+// ReactDOM.render(<Application />, document.getElementById("root"));
