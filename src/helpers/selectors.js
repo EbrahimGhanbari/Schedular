8dd90203c771a06e9638c62bfddeb47d03600bd6
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-07-10 23:23:03 -0600

    get appointment fun tested

---
+++ src/helpers/selectors.js
@@ -0,0 +1,25 @@
+export function getAppointmentsForDay(state, day) {
+
+  // return empty array if days is emppty
+  if (!state.days.length) return [];
+
+  const appointmentsObjectForDay = state.days.filter(stateDay => stateDay.name === day);
+  
+  //return empty array if it can not find the day
+  if (!appointmentsObjectForDay.length) return [];  
+  const appointmentsArrayForDay = appointmentsObjectForDay[0].appointments;
+  
+  const appointmentsArray = [];
+
+  appointmentsArrayForDay.forEach((appointmentDay) => {
+        
+    for (let appoinment in state.appointments) {
+      
+      if (Number(appoinment) === appointmentDay) 
+        appointmentsArray.push(state.appointments[appoinment]);
+    }
+  });
+
+  return appointmentsArray;
+
+}
