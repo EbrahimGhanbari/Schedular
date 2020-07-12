217300ca7ba7ad774121b63f09dadb55287e0575
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-07-11 18:58:19 -0600

    added costum hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export const useVisualMode = (initial) => {
+
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); 
+
+  function transition (newMode, replace = false) {
+
+    setMode(newMode);
+
+    if (replace) history.pop();
+    setHistory((prev) => [...prev, newMode]);
+  }
+
+  function back () {
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      history.pop();
+    }
+
+  }
+ 
+  return { mode, transition, back };
+
+}
